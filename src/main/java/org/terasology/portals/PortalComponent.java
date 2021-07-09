fa68f1945653a2e705436e0b14b7b4d0a4972ed0
DarkWeird <foxflameless@gmail.com>
2021-07-09 16:53:18 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components.

--- src/main/java/org/terasology/portals/PortalComponent.java
+++ src/main/java/org/terasology/portals/PortalComponent.java
@@ -1,21 +1,8 @@
-/*
- * Copyright 2014 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.portals;
 
-import org.terasology.engine.entitySystem.Component;
+import org.terasology.gestalt.entitysystem.component.Component;
 //import org.terasology.model.structures.BlockSelection;
 
 /**
@@ -23,7 +10,7 @@ import org.terasology.engine.entitySystem.Component;
  *
  * @author Rasmus 'Cervator' Praestholm <cervator@gmail.com>
  */
-public class PortalComponent implements Component {
+public class PortalComponent implements Component<PortalComponent> {
     /** The block positions that make up the portal itself (where stuff would spawn / teleport) */
     //private BlockSelection _portalBlocks;
 
