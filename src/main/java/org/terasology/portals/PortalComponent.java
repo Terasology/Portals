44312970eb1523a1d58619480e1e17dc4d39e83d
Michael Pollind <mpollind@gmail.com>
2021-07-11 19:37:58 -0700

    chore: update components

--- src/main/java/org/terasology/portals/PortalComponent.java
+++ src/main/java/org/terasology/portals/PortalComponent.java
@@ -3,7 +3,6 @@
 package org.terasology.portals;
 
 import org.terasology.gestalt.entitysystem.component.Component;
-//import org.terasology.model.structures.BlockSelection;
 
 /**
  * Component for future portal stuff
@@ -11,9 +10,8 @@ import org.terasology.gestalt.entitysystem.component.Component;
  * @author Rasmus 'Cervator' Praestholm <cervator@gmail.com>
  */
 public class PortalComponent implements Component<PortalComponent> {
-    /** The block positions that make up the portal itself (where stuff would spawn / teleport) */
-    //private BlockSelection _portalBlocks;
+    @Override
+    public void copy(PortalComponent other) {
 
-    /** The block positions making up the frame, damage to the frame could break the portal */
-    //private BlockSelection _frameBlocks;
+    }
 }
