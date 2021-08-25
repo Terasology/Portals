66bd0152aea2008e015b35ec9fe734c0dafa47be
Nail Khanipov <foxflameless@gmail.com>
2021-08-25 17:35:32 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components. (#5)
    Ref: MovingBlocks/Terasology#4753
    
    Co-authored-by: Michael Pollind <mpollind@gmail.com>
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- src/main/java/org/terasology/portals/PortalComponent.java
+++ src/main/java/org/terasology/portals/PortalComponent.java
@@ -1,32 +1,17 @@
-/*
- * Copyright 2014 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.portals;
 
-import org.terasology.engine.entitySystem.Component;
-//import org.terasology.model.structures.BlockSelection;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 /**
  * Component for future portal stuff
  *
  * @author Rasmus 'Cervator' Praestholm <cervator@gmail.com>
  */
-public class PortalComponent implements Component {
-    /** The block positions that make up the portal itself (where stuff would spawn / teleport) */
-    //private BlockSelection _portalBlocks;
+public class PortalComponent implements Component<PortalComponent> {
+    @Override
+    public void copyFrom(PortalComponent other) {
 
-    /** The block positions making up the frame, damage to the frame could break the portal */
-    //private BlockSelection _frameBlocks;
+    }
 }
