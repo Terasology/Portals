8be50ab86f18f90e3af0727b0636034fb5decd20
Zangus <wong.thi@gmail.com>
2024-01-03 19:28:34 +0100

    refactor: Components without attributes to EmptyComponent (#7)
    Co-authored-by: thierrywong <wong.t@sfeir.com>

--- src/main/java/org/terasology/portals/PortalComponent.java
+++ src/main/java/org/terasology/portals/PortalComponent.java
@@ -2,16 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.portals;
 
-import org.terasology.gestalt.entitysystem.component.Component;
+import org.terasology.gestalt.entitysystem.component.EmptyComponent;
 
 /**
  * Component for future portal stuff
  *
  * @author Rasmus 'Cervator' Praestholm <cervator@gmail.com>
  */
-public class PortalComponent implements Component<PortalComponent> {
-    @Override
-    public void copyFrom(PortalComponent other) {
-
-    }
+public class PortalComponent extends EmptyComponent<PortalComponent> {
 }
